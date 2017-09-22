4b836a13fe8a05c587861c5486c30dc916ef9ec8
Benjamin Grandey <benjamin.grandey@gmail.com>
2017-09-22 16:33:01 +0800

    Re-writing cdo_analysis_prelim_p17c.sh
    Re-written to produce CDO output to facilitate further analysis.

--- analysis_prelim/cdo_analysis_prelim_p17c.sh
+++ analysis_prelim/cdo_analysis_prelim_p17c.sh
@@ -10,83 +10,118 @@
 # Author:
 #   Benjamin S. Grandey, 2017
 
+date
+
 IN_DIR=$HOME/data/projects/p17c_marc_comparison/output_timeseries/
 OUT_DIR=$HOME/data/projects/p17c_marc_comparison/cdo_analysis_prelim/
 
 # 0. Delete all NetCDF files in output directory
 rm -f $OUT_DIR/*.nc
 
-# 1. Calculatime means across time for years 3-32 for all input files
-for IN_FILE in $IN_DIR/*.nc
+# 1. ANNUAL means for years 3-32 for all fields of interest
+echo "Getting ANNUAL means for years 3-32 for all fields of interest"
+FIELD_LIST=" \
+BURDENBC BURDENPOM BURDENSO4 \
+FSNTOA_d1 FSNTOA FSNTOAC_d1 LWCF SWCF_d1 \
+BC_LDG OC_LDG SUL_LDG \
+CRF DRF FSNTOACNOA FSNTOANOA SAF \
+"
+for FIELD in $FIELD_LIST
 do
-    OUT_FILE="$OUT_DIR/s1.tm.${IN_FILE##*/}"
-    cdo timmean -selyear,3/32 $IN_FILE $OUT_FILE
+    echo $FIELD
+    for IN_FILE in $IN_DIR/p17c_????_????.*.$FIELD.nc
+    do
+	OUT_FILE="$OUT_DIR/s1.ym.${IN_FILE##*/}"
+	# Select just field of interest to avoid multiple grids (which leads to zonmean problems later)
+	TEMP_FILE="$OUT_DIR/temp.nc"
+        cdo selname,$FIELD, $IN_FILE $TEMP_FILE >/dev/null 2>/dev/null
+	# Yearmean for years 3-32
+	cdo yearmean -selyear,3/32 $TEMP_FILE $OUT_FILE >/dev/null 2>/dev/null
+	rm -f $TEMP_FILE
+	echo ${OUT_FILE##*/}
+    done
 done
 
-# 2. 2000-1850 differences
-# 2a. MARC 2D fields
-CASENAME1="p17c_marc_2000"
-CASENAME2="p17c_marc_1850"
-VARIABLE_LIST="FSNTOA FSNTOANOA FSNTOACNOA SAF DRF CRF LWCF"
-
-for VARIABLE in $VARIABLE_LIST
+# 2. Average across LONGITUDES (ie zonal mean)
+echo "Averaging across LONGITUDES"
+for IN_FILE in $OUT_DIR/s1.ym.*.nc
 do
-    IN_FILE1="$OUT_DIR/s1.tm.$CASENAME1.cam.h0.$VARIABLE.nc"
-    IN_FILE2="$OUT_DIR/s1.tm.$CASENAME2.cam.h0.$VARIABLE.nc"
-    OUT_FILE="$OUT_DIR/s2.tm.$CASENAME1-$CASENAME2.cam.h0.$VARIABLE.nc"
-    cdo sub $IN_FILE1 $IN_FILE2 $OUT_FILE
+    OUT_FILE=${IN_FILE/s1/s2.zm}  # bash string replacement
+    cdo zonmean $IN_FILE $OUT_FILE # >/dev/null 2>/dev/null
+    echo ${OUT_FILE##*/}
 done
 
-# 2b. MAM3 2D fields
-CASENAME1="p17c_mam3_2000"
-CASENAME2="p17c_mam3_1850"
-VARIABLE_LIST="FSNTOA FSNTOA_d1 FSNTOAC_d1 SWCF_d1 LWCF LWCF_d1"
-
-for VARIABLE in $VARIABLE_LIST
+# 3. Average across GLOBE (ie area-weighted mean)
+echo "Averaging across GLOBE"
+for IN_FILE in $OUT_DIR/s1.ym.*.nc
 do
-    IN_FILE1="$OUT_DIR/s1.tm.$CASENAME1.cam.h0.$VARIABLE.nc"
-    IN_FILE2="$OUT_DIR/s1.tm.$CASENAME2.cam.h0.$VARIABLE.nc"
-    OUT_FILE="$OUT_DIR/s2.tm.$CASENAME1-$CASENAME2.cam.h0.$VARIABLE.nc"
-    cdo sub $IN_FILE1 $IN_FILE2 $OUT_FILE
+    OUT_FILE=${IN_FILE/s1/s3.fm}
+    cdo fldmean $IN_FILE $OUT_FILE >/dev/null 2>/dev/null
+    echo ${OUT_FILE##*/}
 done
 
-# 2c. Derived fields
-# Net RFP
-for MODEL in marc mam3
+# 4. Average across TIME
+echo "Averaging across TIME"
+for IN_FILE in $OUT_DIR/s1.ym.*.nc
 do
-    IN_FILE1="$OUT_DIR/s2.tm.p17c_${MODEL}_2000-p17c_${MODEL}_1850.cam.h0.FSNTOA.nc"
-    IN_FILE2="$OUT_DIR/s2.tm.p17c_${MODEL}_2000-p17c_${MODEL}_1850.cam.h0.LWCF.nc"
+    OUT_FILE=${IN_FILE/s1/s4.tm}
+    cdo timmean $IN_FILE $OUT_FILE >/dev/null 2>/dev/null
+    echo ${OUT_FILE##*/}
+done
+
+# 5. Average across LONGITUDES & TIME
+echo "Averaging across LONGITUDES & TIME"
+for IN_FILE in $OUT_DIR/s1.ym.*.nc
+do
+    OUT_FILE=${IN_FILE/s1/s5.tm.zm}
+    cdo timmean -zonmean $IN_FILE $OUT_FILE >/dev/null 2>/dev/null
+    echo ${OUT_FILE##*/}
+done
+
+# 6. Average across GLOBE & TIME
+echo "Averaging across GLOBE & TIME"
+for IN_FILE in $OUT_DIR/s1.ym.*.nc
+do
+    OUT_FILE=${IN_FILE/s1/s6.tm.fm}
+    cdo timmean -fldmean $IN_FILE $OUT_FILE >/dev/null 2>/dev/null
+    echo ${OUT_FILE##*/}
+done
+
+# 7. 2000-1850 DIFFERENCES for means across time
+echo "Calculating 2000-1850 DIFFERENCES"
+for IN_FILE1 in $OUT_DIR/s[456].*.p17c_????_1850.*.nc
+do
+    IN_FILE2=${IN_FILE1/1850/2000}
+    OUT_FILE=${IN_FILE1/1850/2000-1850}
+    cdo sub $IN_FILE2 $IN_FILE1 $OUT_FILE >/dev/null 2>/dev/null
+    echo ${OUT_FILE##*/}
+done
+
+# 8. Calculate DERIVED fields
+echo "Calculating DERIVED fields"
+# Net RFP (marc, mam3)
+for IN_FILE1 in $OUT_DIR/s[456].*.p17c_????_2000-1850.*.FSNTOA.nc
+do
+    IN_FILE2=${IN_FILE1/FSNTOA/LWCF}
+    OUT_FILE=${IN_FILE1/FSNTOA/cFNTOA}
     TEMP_FILE="$OUT_DIR/temp.nc"
-    OUT_FILE="$OUT_DIR/s2.tm.p17c_${MODEL}_2000-p17c_${MODEL}_1850.cam.h0.cFNTOA.nc"
-    cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE
-    cdo expr,'cFNTOA=FSNTOA+LWCF' $TEMP_FILE $OUT_FILE
+    cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE >/dev/null 2>/dev/null
+    cdo expr,'cFNTOA=FSNTOA+LWCF' $TEMP_FILE $OUT_FILE >/dev/null 2>/dev/null
     rm -f $TEMP_FILE
+    echo ${OUT_FILE##*/}
 done
-
-# Direct effect - MAM3
-MODEL="mam3"
-IN_FILE1="$OUT_DIR/s2.tm.p17c_${MODEL}_2000-p17c_${MODEL}_1850.cam.h0.FSNTOA.nc"
-IN_FILE2="$OUT_DIR/s2.tm.p17c_${MODEL}_2000-p17c_${MODEL}_1850.cam.h0.FSNTOA_d1.nc"
-TEMP_FILE="$OUT_DIR/temp.nc"
-OUT_FILE="$OUT_DIR/s2.tm.p17c_${MODEL}_2000-p17c_${MODEL}_1850.cam.h0.cDRE.nc"
-cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE
-cdo expr,'cDRE=FSNTOA-FSNTOA_d1' $TEMP_FILE $OUT_FILE
-rm -f $TEMP_FILE
-
-# 2d. Area-weighted means
-for IN_FILE in $OUT_DIR/s2.*.nc
+# Direct effect (mam3)
+for IN_FILE1 in $OUT_DIR/s[456].*.p17c_mam3_2000-1850.*.FSNTOA.nc
 do
-    OUT_FILE=${IN_FILE%nc}fm.nc
-    cdo fldmean $IN_FILE $OUT_FILE
-done
-
-echo "Finished analysis"
-
-# Print area-weighted means
-for IN_FILE in $OUT_DIR/*.fm.nc
-do
-    echo ${IN_FILE##*/}
-    cdo infov $IN_FILE 2>/dev/null | grep -v '0.0000\|nan\|2.1475\|Mean'
+    IN_FILE2=${IN_FILE1/FSNTOA/FSNTOA_d1}
+    OUT_FILE=${IN_FILE1/FSNTOA/cDRE}
+    TEMP_FILE="$OUT_DIR/temp.nc"
+    cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE >/dev/null 2>/dev/null
+    cdo expr,'cDRE=FSNTOA-FSNTOA_d1' $TEMP_FILE $OUT_FILE >/dev/null 2>/dev/null
+    rm -f $TEMP_FILE
+    echo ${OUT_FILE##*/}
 done
 
 echo "Finished"
+
+date
