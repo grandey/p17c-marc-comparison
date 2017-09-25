42ec187ca15d717b10b1ba704a9722c41f1a3e63
Benjamin Grandey <benjamin.grandey@gmail.com>
2017-09-25 16:17:06 +0800

    Continuing preliminary analysis
    - README.org: syncing more data to local machine.
    - cdo_analysis_prelim_p17c.sh: modifying data of interest; and moving
    calculation of derived fields to first step (calculation of annual
    means for years 3-32).
    - analysis_prelim_p17c.ipynb: added; contains function to produce zonal
    mean line plot; applied to produce figures of zonal means for column
    burdens.

--- analysis_prelim/cdo_analysis_prelim_p17c.sh
+++ analysis_prelim/cdo_analysis_prelim_p17c.sh
@@ -21,10 +21,10 @@ rm -f $OUT_DIR/*.nc
 # 1. ANNUAL means for years 3-32 for all fields of interest
 echo "Getting ANNUAL means for years 3-32 for all fields of interest"
 FIELD_LIST=" \
-BURDENBC BURDENPOM BURDENSO4 \
-FSNTOA_d1 FSNTOA FSNTOAC_d1 LWCF SWCF_d1 \
-BC_LDG OC_LDG SUL_LDG \
-CRF DRF FSNTOACNOA FSNTOANOA SAF \
+FSNTOA FSNTOANOA FSNTOACNOA SAF DRF CRF LWCF \
+FSNTOA_d1 FSNTOAC_d1 SWCF_d1
+pSUL_LDG OC_LDG BC_LDG MOS_LDG MBS_LDG \
+BURDENSO4 BURDENPOM BURDENBC \
 "
 for FIELD in $FIELD_LIST
 do
@@ -35,11 +35,38 @@ do
 	# Select just field of interest to avoid multiple grids (which leads to zonmean problems later)
 	TEMP_FILE="$OUT_DIR/temp.nc"
         cdo selname,$FIELD, $IN_FILE $TEMP_FILE >/dev/null 2>/dev/null
-	# Yearmean for years 3-32
+	# Yearmean for years 3-32 - with year starting in December of previous year
 	cdo yearmean -selyear,3/32 $TEMP_FILE $OUT_FILE >/dev/null 2>/dev/null
 	rm -f $TEMP_FILE
 	echo ${OUT_FILE##*/}
     done
+done
+
+# 1b. Calculate DERIVED fields
+echo "Calculating DERIVED fields"
+# Net RFP (marc, mam3) - when 2000-1850 difference is calculated
+for IN_FILE1 in $OUT_DIR/s1.ym.p17c_marc_????.*.FSNTOA.nc $OUT_DIR/s1.*.p17c_mam3_????.*.FSNTOA.nc
+do
+    echo "cFNTOA"
+    IN_FILE2=${IN_FILE1/FSNTOA/LWCF}
+    OUT_FILE=${IN_FILE1/FSNTOA/cFNTOA}
+    TEMP_FILE="$OUT_DIR/temp.nc"
+    cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE >/dev/null 2>/dev/null
+    cdo expr,'cFNTOA=FSNTOA+LWCF' $TEMP_FILE $OUT_FILE >/dev/null 2>/dev/null
+    rm -f $TEMP_FILE
+    echo ${OUT_FILE##*/}
+done
+# Direct effect (mam3)
+for IN_FILE1 in $OUT_DIR/s1.ym.p17c_mam3_????.*.FSNTOA.nc
+do
+    echo "cDRE"
+    IN_FILE2=${IN_FILE1/FSNTOA/FSNTOA_d1}
+    OUT_FILE=${IN_FILE1/FSNTOA/cDRE}
+    TEMP_FILE="$OUT_DIR/temp.nc"
+    cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE >/dev/null 2>/dev/null
+    cdo expr,'cDRE=FSNTOA-FSNTOA_d1' $TEMP_FILE $OUT_FILE >/dev/null 2>/dev/null
+    rm -f $TEMP_FILE
+    echo ${OUT_FILE##*/}
 done
 
 # 2. Average across LONGITUDES (ie zonal mean)
@@ -97,30 +124,30 @@ do
     echo ${OUT_FILE##*/}
 done
 
-# 8. Calculate DERIVED fields
-echo "Calculating DERIVED fields"
-# Net RFP (marc, mam3)
-for IN_FILE1 in $OUT_DIR/s[456].*.p17c_????_2000-1850.*.FSNTOA.nc
-do
-    IN_FILE2=${IN_FILE1/FSNTOA/LWCF}
-    OUT_FILE=${IN_FILE1/FSNTOA/cFNTOA}
-    TEMP_FILE="$OUT_DIR/temp.nc"
-    cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE >/dev/null 2>/dev/null
-    cdo expr,'cFNTOA=FSNTOA+LWCF' $TEMP_FILE $OUT_FILE >/dev/null 2>/dev/null
-    rm -f $TEMP_FILE
-    echo ${OUT_FILE##*/}
-done
-# Direct effect (mam3)
-for IN_FILE1 in $OUT_DIR/s[456].*.p17c_mam3_2000-1850.*.FSNTOA.nc
-do
-    IN_FILE2=${IN_FILE1/FSNTOA/FSNTOA_d1}
-    OUT_FILE=${IN_FILE1/FSNTOA/cDRE}
-    TEMP_FILE="$OUT_DIR/temp.nc"
-    cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE >/dev/null 2>/dev/null
-    cdo expr,'cDRE=FSNTOA-FSNTOA_d1' $TEMP_FILE $OUT_FILE >/dev/null 2>/dev/null
-    rm -f $TEMP_FILE
-    echo ${OUT_FILE##*/}
-done
+# # 8. Calculate DERIVED fields
+# echo "Calculating DERIVED fields"
+# # Net RFP (marc, mam3)
+# for IN_FILE1 in $OUT_DIR/s[456].*.p17c_????_2000-1850.*.FSNTOA.nc
+# do
+#     IN_FILE2=${IN_FILE1/FSNTOA/LWCF}
+#     OUT_FILE=${IN_FILE1/FSNTOA/cFNTOA}
+#     TEMP_FILE="$OUT_DIR/temp.nc"
+#     cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE >/dev/null 2>/dev/null
+#     cdo expr,'cFNTOA=FSNTOA+LWCF' $TEMP_FILE $OUT_FILE >/dev/null 2>/dev/null
+#     rm -f $TEMP_FILE
+#     echo ${OUT_FILE##*/}
+# done
+# # Direct effect (mam3)
+# for IN_FILE1 in $OUT_DIR/s[456].*.p17c_mam3_2000-1850.*.FSNTOA.nc
+# do
+#     IN_FILE2=${IN_FILE1/FSNTOA/FSNTOA_d1}
+#     OUT_FILE=${IN_FILE1/FSNTOA/cDRE}
+#     TEMP_FILE="$OUT_DIR/temp.nc"
+#     cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE >/dev/null 2>/dev/null
+#     cdo expr,'cDRE=FSNTOA-FSNTOA_d1' $TEMP_FILE $OUT_FILE >/dev/null 2>/dev/null
+#     rm -f $TEMP_FILE
+#     echo ${OUT_FILE##*/}
+# done
 
 echo "Finished"
 
